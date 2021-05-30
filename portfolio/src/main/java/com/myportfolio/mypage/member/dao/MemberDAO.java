50cd59a239bb27fd61cc98cef542eadf22cc6369
th1993kim <81105748+th1993kim@users.noreply.github.com>
2021-05-30 19:57:13 +0900

    Delete directory

--- portfolio/src/main/java/com/myportfolio/mypage/member/dao/MemberDAO.java
+++
@@ -1,9 +0,0 @@
-package com.myportfolio.mypage.member.dao;
-
-import org.springframework.dao.DataAccessException;
-
-import com.myportfolio.mypage.member.dto.MemberDTO;
-
-public interface MemberDAO {
-	public MemberDTO login(MemberDTO memberDto) throws DataAccessException;
-}
--- portfolio/src/main/java/com/myportfolio/mypage/member/service/MemberService.java
+++
@@ -1,7 +0,0 @@
-package com.myportfolio.mypage.member.service;
-
-import com.myportfolio.mypage.member.dto.MemberDTO;
-
-public interface MemberService {
-	public MemberDTO login(MemberDTO memberDto) throws Exception;
-}
